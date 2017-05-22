d8333c198df8e73fbd38b1aa1edb7cfc00e5f6f1
loren pabst <loren pabst>
2017-05-22 08:59:33 -0600

    tested db then added folder and file structure for front end

---
+++ public/app.js
@@ -0,0 +1,33 @@
+angular.module('**********NAME HERE*************', ['ui.router'])
+.config(function($urlRouterProvider, $stateProvider){
+    
+
+$stateProvider
+    .state('', {
+        url: '',
+        templateUrl: '',
+        controller: '',
+    })
+    .state('', {
+        url: '',
+        templateUrl: '',
+        controller: '',
+    })
+    .state('', {
+        url: '',
+        templateUrl: '',
+        controller: '',
+    })
+    .state('', {
+        url: '',
+        templateUrl: '',
+        controller: '',
+    })
+
+
+$urlRouterProvider.otherwise('/');
+
+
+
+
+});
