bdab2ddb275289659f952ab856475089937c0806
loren pabst <loren pabst>
2017-05-23 11:24:44 -0600

    added routes to angular app

--- public/app.js
+++ public/app.js
@@ -3,25 +3,40 @@ angular.module('**********NAME HERE*************', ['ui.router'])
     
 
 $stateProvider
-    .state('', {
-        url: '',
-        templateUrl: '',
+    .state('home', {
+        url: '/',
+        templateUrl: './app/routes/home/home.html',
         controller: '',
     })
-    .state('', {
-        url: '',
-        templateUrl: '',
+    .state('contact', {
+        url: '/contact',
+        templateUrl: './app/routes/contact/contact.html',
         controller: '',
     })
-    .state('', {
-        url: '',
-        templateUrl: '',
+    .state('angular', {
+        url: '/angular',
+        templateUrl: './app/routes/angular/angular.html',
+        controller: 'angularCtrl',
+    })
+    .state('css', {
+        url: '/css',
+        templateUrl: './app/routes/cssPage/cssPage.html',
         controller: '',
     })
-    .state('', {
-        url: '',
-        templateUrl: '',
-        controller: '',
+    .state('greensock', {
+        url: '/greensock',
+        templateUrl: './app/routes/greensock/greensock.html',
+        controller: 'greensockCtrl',
+    })
+    .state('jquery', {
+        url: '/jquery',
+        templateUrl: './app/routes/jquery/jquery.html',
+        controller: 'jqueryCtrl',
+    })
+    .state('javascript', {
+        url: '/javascript',
+        templateUrl: './app/routes/vanillaJS/vanillaJS.html',
+        controller: 'javascriptCtrl',
     })
 
 
