823527d9d05e2ff353fdfa0e779ece2b90f57eb2
loren pabst <loren pabst>
2017-05-24 12:19:02 -0600

    header is very responsive now

--- dist/bundle.js
+++ dist/bundle.js
@@ -70,7 +70,30 @@ angular.module('tutorialSite').controller('mainCtrl', ["$scope", function($scope
         $scope.mobileMenu = false;
     }
 
-
+    function changeHeaderCss(){
+        var dHeader = $('.desktop_header');
+        if (this.scrollY > 50){
+            dHeader.css({
+                "width": "100%",
+                'position': 'fixed',
+                'left': '0',
+                'top': '0',
+                'border-radius': '0'
+            });
+            $('.desktop_nav').css('right', '2.3%');
+        }else{
+            dHeader.css({
+                "width": "94%",
+                'position': 'absolute',
+                'left': '3%',
+                'top': '20px',
+                'border-radius': '6px'
+            });
+            $('.desktop_nav').css('right', '0%');
+        }
+    }
+    
+    window.addEventListener("scroll", changeHeaderCss, false);
 
 
 
