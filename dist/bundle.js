4f86afab9977e827518ae83c020b65c883c5647d
loren pabst <loren pabst>
2017-05-30 12:33:42 -0600

    jquery example 1 is working

--- dist/bundle.js
+++ dist/bundle.js
@@ -130,52 +130,6 @@ angular.module('tutorialSite').controller('mainCtrl', ["$scope", function($scope
 
 }]);
 
-angular.module('tutorialSite').controller('vanillaJSCtrl', ["$scope", function($scope){
-
-    var box1 = document.getElementById('js_box1');
-    var box2 = document.getElementById('js_box2');
-    var box3 = document.getElementById('js_box3');
-    var box4 = document.getElementById('js_box4');
-    var input = document.getElementById('js_input');
-    var submitBtn = document.getElementById('js_submit');
-
-    box1.addEventListener('click', function (){
-        if (box1.style.background == 'red'){
-            box1.style.background = 'blue';
-        }else{
-            box1.style.background = 'red';
-        }
-    });
-
-    box2.addEventListener('mouseover', function(){
-        box2.style.position = 'absolute';
-        box2.style.left = '50%';
-    });
-
-    box3.addEventListener('mouseover', function(){
-        box3.style.position = 'absolute';
-        if (box3.style.left == '50%'){
-            box3.style.left = '10px';
-        }else{
-            box3.style.left = '50%';
-        }
-    });
-
-    submitBtn.addEventListener('click', function(){
-        box4.innerText = input.value;
-    });
-
-
-
-
-}])
-
-
-
-
-
-
-
 angular.module('tutorialSite').controller('homeCtrl', ["$scope", "homeService", function($scope, homeService){
 
     $scope.getHomePageIntro = function() {
@@ -277,3 +231,89 @@ angular.module('tutorialSite').service('homeService', ["$http", function($http){
 
 
 }]);
+angular.module('tutorialSite').controller('jqueryCtrl', ["$scope", function($scope){
+
+    // $(document).ready(function(){
+
+        var box1 = $('#jquery_box1');
+        var box2 = $('#jquery_box2');
+        var box3 = $('#jquery_box3');
+        var box4 = $('#jquery_box4');
+
+        box1.click(function(){
+            if($(this).css('height') == '40px'){
+                box1.css({
+                    'height': '80px',
+                    'width': '300px'
+                })
+            }else{
+                box1.css({
+                    'height': '40px',
+                    'width': '140px'
+                })
+            }
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+    // })
+
+
+
+
+
+}]);
+angular.module('tutorialSite').controller('vanillaJSCtrl', ["$scope", function($scope){
+
+    var box1 = document.getElementById('js_box1');
+    var box2 = document.getElementById('js_box2');
+    var box3 = document.getElementById('js_box3');
+    var box4 = document.getElementById('js_box4');
+    var input = document.getElementById('js_input');
+    var submitBtn = document.getElementById('js_submit');
+
+    box1.addEventListener('click', function (){
+        if (box1.style.background == 'red'){
+            box1.style.background = 'blue';
+        }else{
+            box1.style.background = 'red';
+        }
+    });
+
+    box2.addEventListener('mouseover', function(){
+        box2.style.position = 'absolute';
+        box2.style.left = '50%';
+    });
+
+    box3.addEventListener('mouseover', function(){
+        box3.style.position = 'absolute';
+        if (box3.style.left == '50%'){
+            box3.style.left = '10px';
+        }else{
+            box3.style.left = '50%';
+        }
+    });
+
+    submitBtn.addEventListener('click', function(){
+        box4.innerText = input.value;
+    });
+
+
+
+
+}])
+
+
+
+
+
+
--- public/app/routes/jquery/jqueryCtrl.js
+++ public/app/routes/jquery/jqueryCtrl.js
@@ -1,7 +1,38 @@
 angular.module('tutorialSite').controller('jqueryCtrl', function($scope){
 
+    // $(document).ready(function(){
+
+        var box1 = $('#jquery_box1');
+        var box2 = $('#jquery_box2');
+        var box3 = $('#jquery_box3');
+        var box4 = $('#jquery_box4');
+
+        box1.click(function(){
+            if($(this).css('height') == '40px'){
+                box1.css({
+                    'height': '80px',
+                    'width': '300px'
+                })
+            }else{
+                box1.css({
+                    'height': '40px',
+                    'width': '140px'
+                })
+            }
+        })
 
 
+
+
+
+
+
+
+
+
+
+
+    // })
 
 
 
