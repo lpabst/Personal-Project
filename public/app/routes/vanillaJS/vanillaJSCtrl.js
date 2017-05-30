9aa20174177d68d1377c1fefabc78f2dbb9d3615
loren pabst <loren pabst>
2017-05-29 21:13:12 -0600

    added more to the js page

--- public/app/routes/vanillaJS/vanillaJSCtrl.js
+++ public/app/routes/vanillaJS/vanillaJSCtrl.js
@@ -1,16 +1,30 @@
 angular.module('tutorialSite').controller('vanillaJSCtrl', function($scope){
 
     var box1 = document.getElementById('js_examplebox1');
+    var box2 = document.getElementById('js_examplebox2');
+    var box3 = document.getElementById('js_examplebox3');
+
     box1.addEventListener('click', function (){
         if (box1.style.background == 'red'){
             box1.style.background = 'blue';
         }else{
             box1.style.background = 'red';
         }
-    })
+    });
 
+    box2.addEventListener('mouseover', function(){
+        box2.style.position = 'absolute';
+        box2.style.left = '300px';
+    });
 
-
+    box3.addEventListener('mouseover', function(){
+        box3.style.position = 'absolute';
+        if (box3.style.left == '300px'){
+            box3.style.left = '10px';
+        }else{
+            box3.style.left = '300px';
+        }
+    });
 
 
 
