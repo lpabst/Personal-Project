b451532e32347de5f493ffec6db43bef3b27c77e
loren pabst <loren pabst>
2017-05-30 14:47:58 -0600

    building jquery carousel

--- dist/bundle.js
+++ dist/bundle.js
@@ -130,47 +130,6 @@ angular.module('tutorialSite').controller('mainCtrl', ["$scope", function($scope
 
 }]);
 
-angular.module('tutorialSite').controller('jqueryCtrl', ["$scope", function($scope){
-
-    // $(document).ready(function(){
-
-        var box1 = $('#jquery_box1');
-        var box2 = $('#jquery_box2');
-        var box3 = $('#jquery_box3');
-        var box4 = $('#jquery_box4');
-
-        box1.click(function(){
-            if($(this).css('height') == '40px'){
-                box1.css({
-                    'height': '80px',
-                    'width': '300px'
-                })
-            }else{
-                box1.css({
-                    'height': '40px',
-                    'width': '140px'
-                })
-            }
-        })
-
-
-
-
-
-
-
-
-
-
-
-
-    // })
-
-
-
-
-
-}]);
 angular.module('tutorialSite').controller('homeCtrl', ["$scope", "homeService", function($scope, homeService){
 
     $scope.getHomePageIntro = function() {
@@ -269,6 +228,77 @@ angular.module('tutorialSite').service('homeService', ["$http", function($http){
   this.getTechInfo = function(){
       return techInfo;
   }
+
+
+}]);
+angular.module('tutorialSite').controller('jqueryCtrl', ["$scope", function($scope){
+
+    // $(document).ready(function(){
+
+    var box1 = $('#jquery_box1');
+    var box2 = $('#jquery_box2');
+    var height = $('#jquery_input_height');
+    var width = $('#jquery_input_width');
+    var prev = $('.prev');
+    var next = $('.next');
+    var pics = $('#jquery_carousel_pictures');
+
+//example 1 - changes box width and height
+    box1.click(function(){
+        if($(this).css('height') == '40px'){
+            box1.css({
+                'height': '80px',
+                'width': '300px'
+            })
+        }else{
+            box1.css({
+                'height': '40px',
+                'width': '140px'
+            })
+        }
+    })
+
+// Example2 - Changes box width and height based on user input
+    box2.click(function(){
+        if (height.val() < 10 || width.val() < 10 || height.val() > 300 || width.val() > 300){
+            alert('Please enter height and width values between 10-300');
+        }else{
+            box2.css({
+                'width': width.val() + 'px',
+                'height': height.val() + 'px'
+            })
+        }
+    })
+
+// Example3 - JQuery Carousel. Changes the positioning of the div so 
+// that a different picture is showing.
+    prev.click(function(){
+        var left = pics.css('left') - 20;
+        console.log(left);
+        pics.css({
+            left: left + '%'
+        })
+    });
+
+    next.click(function(){
+        var left = pics.css('left') + 20;
+        console.log(left);
+        pics.css({
+            left: left + '%'
+        })
+    });
+
+
+
+
+
+
+
+
+    // })
+
+
+
 
 
 }]);
--- public/app/routes/jquery/jqueryCtrl.js
+++ public/app/routes/jquery/jqueryCtrl.js
@@ -2,28 +2,58 @@ angular.module('tutorialSite').controller('jqueryCtrl', function($scope){
 
     // $(document).ready(function(){
 
-        var box1 = $('#jquery_box1');
-        var box2 = $('#jquery_box2');
-        var box3 = $('#jquery_box3');
-        var box4 = $('#jquery_box4');
+    var box1 = $('#jquery_box1');
+    var box2 = $('#jquery_box2');
+    var height = $('#jquery_input_height');
+    var width = $('#jquery_input_width');
+    var prev = $('.prev');
+    var next = $('.next');
+    var pics = $('#jquery_carousel_pictures');
 
-        box1.click(function(){
-            if($(this).css('height') == '40px'){
-                box1.css({
-                    'height': '80px',
-                    'width': '300px'
-                })
-            }else{
-                box1.css({
-                    'height': '40px',
-                    'width': '140px'
-                })
-            }
+//example 1 - changes box width and height
+    box1.click(function(){
+        if($(this).css('height') == '40px'){
+            box1.css({
+                'height': '80px',
+                'width': '300px'
+            })
+        }else{
+            box1.css({
+                'height': '40px',
+                'width': '140px'
+            })
+        }
+    })
+
+// Example2 - Changes box width and height based on user input
+    box2.click(function(){
+        if (height.val() < 10 || width.val() < 10 || height.val() > 300 || width.val() > 300){
+            alert('Please enter height and width values between 10-300');
+        }else{
+            box2.css({
+                'width': width.val() + 'px',
+                'height': height.val() + 'px'
+            })
+        }
+    })
+
+// Example3 - JQuery Carousel. Changes the positioning of the div so 
+// that a different picture is showing.
+    prev.click(function(){
+        var left = pics.css('left') - 20;
+        console.log(left);
+        pics.css({
+            left: left + '%'
         })
+    });
 
-
-
-
+    next.click(function(){
+        var left = pics.css('left') + 20;
+        console.log(left);
+        pics.css({
+            left: left + '%'
+        })
+    });
 
 
 
