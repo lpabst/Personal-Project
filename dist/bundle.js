68e0b7dd67fc12b8dcfd9187cac70643d4361176
loren pabst <loren pabst>
2017-06-01 15:28:58 -0600

    added more examples to greensock page

--- dist/bundle.js
+++ dist/bundle.js
@@ -138,13 +138,41 @@ angular.module('tutorialSite').controller('angularCtrl', ["$scope", function($sc
 }]);
 angular.module('tutorialSite').controller('greensockCtrl', ["$scope", function($scope){
 
+    var box2 = $('#greensock_box2');
+    var box3 = $('#greensock_box3');
+    let box4_btn = $('#gs_example4_btn');
+    var box4 = $('#greensock_box4');
 
+    TweenLite.to($('#greensock_box1'), 10, {left:'200px'});
 
+    box2.click(function(){
+        TweenLite.to(box2, 2, {top:'130px'});
+        TweenLite.to(box2, 2, {left:'200px', delay:2});
+        TweenLite.to(box2, 2, {top:'0px', left:'160px', delay:4});
+        TweenLite.to(box2, 2, {top:'33px', left:'0px', delay:6});
+    })
 
+    function firstAnimation(){
+        TweenLite.to(box3, 1, {
+            left:'150px', 
+            onComplete:secondAnimation
+        });
+    }
 
+    function secondAnimation(){
+        TweenLite.to(box3, 1, {
+            left:'0px', 
+            onComplete:firstAnimation
+        });
+    }
 
+    box3.click(function(){
+        firstAnimation();
+    })
 
-
+    box4_btn.click(function(){
+        TweenLite.from(box4, 1.2, {left:'200px'});
+    })
 
 
 }]);
