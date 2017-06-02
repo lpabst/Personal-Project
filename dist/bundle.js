0e03bbd8c008fbe281cfa95103bc22d7a6a73552
loren pabst <loren pabst>
2017-06-02 09:46:02 -0600

    fixed some styling and added more animation to the greensock plant example

--- dist/bundle.js
+++ dist/bundle.js
@@ -143,6 +143,8 @@ angular.module('tutorialSite').controller('greensockCtrl', ["$scope", function($
     let box4_btn = $('#gs_example4_btn');
     var box4 = $('#greensock_box4');
     var plantExampleBtn = $('#plant_example_btn');
+    var plant_reset_btn = $('#plant_reset_btn');
+    var sun = $('.sun');
     var stem = $('.stem');
     var petals = $('.petal');
     var leaf = $('.leaf');
@@ -180,11 +182,30 @@ angular.module('tutorialSite').controller('greensockCtrl', ["$scope", function($
     })
 
     plantExampleBtn.click(function(){
+        //sun
+        var mq = window.matchMedia( "(min-width: 1000px)" );
+        if(mq.matches){//Checks if the screen is larger than 1000px or not
+            TweenLite.to(sun, 3.4, {left:'480px', top:'15px', ease:Linear.easeNone});
+            TweenLite.to(sun, 3.6, {left:'960px', top:'65px', delay:3.4});
+        }else{
+            TweenLite.to(sun, 3.4, {left:'150px', top:'15px'});
+            TweenLite.to(sun, 3.6, {left:'300px', top:'65px', delay:3.4});
+        }
+        
+        //plant
         TweenLite.to(stem, 5, {height:'120px'});
         TweenLite.to(leaf, 3, {width: '30px', height: '18px', delay:1});
         TweenLite.to(miniStem, 3, {width:'4px', height:'42px', delay:1.8});
         TweenLite.to(petals, 0.1, {border:'0.5px solid gray', delay:4.7});
         TweenLite.to(petals, 2, {width:'14px', height:'20px', left:'-3px', delay:4.8});
+    })
+
+    plant_reset_btn.click(function(){
+        TweenLite.to(sun, 1, {left:'45px', top:'65px'});
+        TweenLite.to(stem, 1, {height:'30px'});
+        TweenLite.to(leaf, 1, {width: '10px', height: '6px'});
+        TweenLite.to(miniStem, 1, {width:'3px', height:'0px'});
+        TweenLite.to(petals, 1, {width:'0px', height:'0px', left:'0px', border:'0px solid gray'});
     })
 }]);
 angular.module('tutorialSite').controller('homeCtrl', ["$scope", "homeService", function($scope, homeService){
